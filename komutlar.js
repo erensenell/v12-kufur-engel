4ce066a62b30c081056dea47a0a2ef00018c92af
erenşenel <93876407+erensenell@users.noreply.github.com>
2022-05-04 15:05:12 +0300

    Add files via upload

---
+++ komutlar.js
@@ -0,0 +1,95 @@
+client.on("message", async msg => {
+  const i = await db.fetch(`${msg.guild.id}.kufur`);
+  if (i) {
+    const kufur = [
+      "oç",
+      "amk",
+      "ananı sikiyim",
+      "ananıskm",
+      "piç",
+      "amk",
+      "amsk",
+      "sikim",
+      "sikiyim",
+      "orospu çocuğu",
+      "piç kurusu",
+      "kahpe",
+      "orospu",
+      "mal",
+      "sik",
+      "yarrak",
+      "am",
+      "amcık",
+      "amık",
+      "yarram",
+      "sikimi ye",
+      "mk",
+      "mq",
+      "aq",
+      "ak",
+      "amq"
+    ];
+    if (kufur.some(word => msg.content.includes(word))) {
+      try {
+        if (!msg.member.permissions.has("BAN_MEMBERS")) {
+          msg.delete();
+
+          return msg
+            .reply("Heey! Küfür Yasak.")
+            .then(wiskyx => wiskyx.delete({ timeout: 5000 }));
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  if (!i) return;
+});
+
+client.on("messageUpdate", async msg => {
+  const i = db.fetch(`${msg.guild.id}.kufur`);
+  if (i) {
+    const kufur = [
+      "oç",
+      "amk",
+      "ananı sikiyim",
+      "ananıskm",
+      "piç",
+      "amk",
+      "amsk",
+      "sikim",
+      "sikiyim",
+      "orospu çocuğu",
+      "piç kurusu",
+      "kahpe",
+      "orospu",
+      "mal",
+      "sik",
+      "yarrak",
+      "am",
+      "amcık",
+      "amık",
+      "yarram",
+      "sikimi ye",
+      "mk",
+      "mq",
+      "aq",
+      "ak",
+      "amq"
+    ];
+    if (kufur.some(word => msg.content.includes(word))) {
+      try {
+        if (!msg.member.permissions.has("BAN_MEMBERS")) {
+          msg.delete();
+
+          return msg
+            .reply("Yakaladım Seni! Küfür Yasak.")
+            .then(wiskyx => wiskyx.delete({ timeout: 5000 }));
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  if (!i) return;
+});
